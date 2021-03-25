6060ba2f5671cb63e0fa93d7241fdd7619e021f2
Natalia2525 <65954006+Natalia2525@users.noreply.github.com>
2021-03-25 21:21:46 +0200

    add selectors

--- src/Components/ContactList/ContactList.jsx
+++ src/Components/ContactList/ContactList.jsx
@@ -2,10 +2,7 @@ import ContactItem from './ContactItem';
 // import PropTypes from 'prop-types';
 import { createUseStyles } from 'react-jss';
 import { useSelector } from 'react-redux';
-import {
-  getContacts,
-  getFilter,
-} from '../../redux/contacts/contacts-selectors';
+import { getVisibleContacts } from '../../redux/contacts/contacts-selectors';
 
 const useStyles = createUseStyles({
   list: {
@@ -15,20 +12,13 @@ const useStyles = createUseStyles({
 });
 
 const ContactList = () => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
+  
+  const contacts = useSelector(getVisibleContacts);
   const classes = useStyles();
-
-  const getVisibleContacts = !filter
-    ? contacts
-    : contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter.toLocaleLowerCase()),
-      );
 
   return (
     <ul className={classes.list}>
-      {getVisibleContacts.map(contact => (
+      {contacts.map(contact => (
         <ContactItem key={contact.id} contact={contact} />
       ))}
     </ul>
--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,2 +1,14 @@
 export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
+
+export const getVisibleContacts = state => {
+  // const contacts = state.contacts.items;
+  const contacts = getContacts(state);
+
+  // const filter = state.contacts.filter;
+  const filter = getFilter(state);
+
+  return contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLocaleLowerCase()),
+  );
+};
